479226424ab52dae2814246fd8e7c513e8ec9333
ignasstephanie <ignasstephanie@gmail.com>
2021-07-27 15:42:47 +0800

    removeSubmit

--- resources/views/students/studentList.blade.php
+++ resources/views/students/studentList.blade.php
@@ -100,9 +100,6 @@
                     <label for="exampleFormControlInput1" class="form-label">Address</label>
                     <input type="text" class="form-control" name="address" id="editaddress" >
                 </div>
-                <div class="mb-3">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                     <button type="submit" class="btn btn-primary">Update</button>
